9d028ec11b6434dfbe6c8d7edce9fe7b8b1c4f0a
Kevin Alberts <kevin@kevinalberts.nl>
2016-01-22 01:47:05 +0100

    swipe: Added basic internationalization for english and dutch, and added translations for those languages.

--- swipe/settings.py
+++ swipe/settings.py
@@ -113,6 +113,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+# How to generate locale files:
+# https://docs.djangoproject.com/en/1.9/topics/i18n/translation/#localization-how-to-create-language-files
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
