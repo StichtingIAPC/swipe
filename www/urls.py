7a74943b7587b99a3a5e4a53df8143eeab27df1a
Kevin Alberts <kevin@kevinalberts.nl>
2016-01-29 00:12:18 +0100

    swipe: Added basic user and permission support based on Django's default implementation. Made the supplier page logged-in only and only editable with permissions as a demonstration.

--- www/urls.py
+++ www/urls.py
@@ -3,11 +3,17 @@ WWW URL Configuration
 """
 
 from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 from www.views import home
 
 urlpatterns = [
     # Home page
     url(r'^$', home, name='home'),
+
+    # Authentication URLs
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^switch_user/$', auth_views.logout_then_login, name='switch_user'),
 
     # Include Supplier URLs
     url(r'^supplier/', include('supplier.urls')),
