71707e24789c284c2939f573e353ad3421c9ae23
Nander Voortman <nander@nander.org>
2016-01-29 11:20:48 +0100

    Test simplified, by adding a name to the TestMoneyType.

--- money/tests.py
+++ money/tests.py
@@ -20,23 +20,12 @@ class MoneyTest(TestCase):
         c = Currency.objects.create(name="EURO",iso="EUR",symbol="$")
         m = Money(amount=5.21,currency=c)
 
-        t = TestMoneyType.objects.create(money=m)
+        t = TestMoneyType.objects.create(money=m, name="Some Reason")
         print(t.money.currency)
 
     def testSymbol(self):
-        vv = Currency.objects.get(name="EURO")
-        val = TestMoneyType.objects.all()
-        i=0
-        for v in val:
-            self.assertEqual(v.money.amount.__str__(), "5.21000")
-            self.assertEqual(v.money.currency,vv)
+        vv = Currency.objects.get(iso="EUR")
+        val = TestMoneyType.objects.get(name="Some Reason")
+        self.assertEqual(val.money.amount.__str__(), "5.21000")
+        self.assertEqual(val.money.currency,vv)
 
-
-        self.assertEqual(vv.iso,"EUR")
- #   def test_Euro(self):
-  #      """Animals that can speak are correctly identified"""
-#        val = TestMoneyType.objects.all()
-#        i = 0
-#        for v in val:
-#            i += 1
-#        self.assertEqual(i,1)
