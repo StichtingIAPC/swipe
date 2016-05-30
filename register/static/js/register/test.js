64144f3b2ea3e701d71601a23926152a85d5fe74
Nander Voortman <nvoortman@iapc.utwente.nl>
2016-05-30 14:51:07 +0200

    Update test.js

--- register/static/js/register/test.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by nvoortman on 21-4-2016.
- */
-    a = document.querySelectorAll("*[register='Cash']")
-    for (l of a){
-        console.log(l.getAttribute("denomination"))
-
-    }
