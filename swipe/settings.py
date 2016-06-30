257debec64c6287f800b28365c67a873b9f31bd6
Matthias van de Meent <mmeent@iapc.utwente.nl>
2016-06-30 22:38:58 +0200

    Update settings.py, as this change was accidentally pushed by Leon.

--- swipe/settings.py
+++ swipe/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(&-^3hx6os!h^sxu(v2py(78zuqtp8pxtu_3f=jh$+c4hw1taw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
