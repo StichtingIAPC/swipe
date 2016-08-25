f43a6db0dd3d8c41e8832789f802bfcea1b2feb9
Nander Voortman <nvoortman@iapc.utwente.nl>
2016-08-25 23:41:42 +0200

    Update settings.py

--- swipe/settings.py
+++ swipe/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'swipe.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '2db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
