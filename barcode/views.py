b4b6d2336e5aedd0de0f9144b94766c6d415de5c
nanderv <nanderv@gmail.com>
2016-09-14 09:49:22 +0200

    Spaties!!!

--- barcode/views.py
+++ barcode/views.py
@@ -4,12 +4,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from reportlab.graphics.barcode import createBarcodeDrawing
 
-from barcode.models import MyBarcodeDrawing
 # Create your views here.
 
 
-
-def qr(request,str):
+def qr(request, str):
     """
         Create QR code for random data
     """
@@ -22,22 +20,22 @@ def qr(request,str):
     return HttpResponse(binary_stuff, 'image/gif')
 
 
-def qr_url(request,str):
+def qr_url(request, str):
     """
         Create QR Code for a url within swipe
     """
     return qr(request, "https://s.iapc.utwente.nl/c/"+str)
 
 
-def barcode(request,str):
+def barcode(request, str):
     """
         Create BAR code for random data, please keep inputs of reasonable length
     """
     d = createBarcodeDrawing('Code128', value=str, humanReadable=True)
-    d.width *= 5
-    d.height *= 5
-    d.translate(0,32)
-    d.scale(5,5)
+    d.width *= 3
+    d.height *= 3
+    d.translate(0,24)
+    d.scale(3,3)
     binary_stuff = d.asString("gif")
     return HttpResponse(binary_stuff, 'image/gif')
 
