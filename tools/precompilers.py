954f4444f98aa2162b4ceb6f472985ae6f1a8cbb
mattashii <boekewurm@gmail.com>
2016-09-22 03:33:47 +0200

    Now styles and js should be cached. Look at local.py.default for extra settings when you are writing styles or writing js code.

--- tools/precompilers.py
+++ tools/precompilers.py
@@ -4,13 +4,11 @@ from compressor_toolkit.precompilers import SCSSCompiler, ES6Compiler
 
 class CachedES6Compiler(CachedCompilerFilter, ES6Compiler):
     def __init__(self, *args, **kwargs):
-        print(CachedES6Compiler.__mro__)
-        super().__init__(*args, content=None, **kwargs)
-        self.mimetype = "module"
+        super().__init__(*args, content=None, **kwargs)  # Content has to be given to CompilerFilter, but it doesn't do anything
+        self.mimetype = "module"  # nessesary for mimetype checks in CachedCompiler
 
 
 class CachedSCSSCompiler(CachedCompilerFilter, SCSSCompiler):
     def __init__(self, *args, **kwargs):
-        print(CachedSCSSCompiler.__mro__)
-        super().__init__(*args, content=None, **kwargs)
-        self.mimetype = "text/x-scss"
+        super().__init__(*args, content=None, **kwargs)  # Content has to be given to CompilerFilter, but it doesn't do anything
+        self.mimetype = "text/x-scss"  # nessesary for mimetype checks in CachedCompiler
