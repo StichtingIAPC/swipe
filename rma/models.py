cc838fc671941e2692e447fa2799fa74dfed392d
Léon van der Kaap <leonkaap@gmail.com>
2016-10-02 00:58:50 +0200

    New ideas on the RMA model

--- rma/models.py
+++ rma/models.py
@@ -8,7 +8,7 @@ class RMA(Blame):
     # The customer. Null for ourselves as customer
     customer = models.ForeignKey(Customer, null=True)
     # The state in which the RMA is
-    state = models.CharField(max_length=3)
+
     # The RMA-number. Null for no
     rma_number = models.CharField(max_length=60, null=True)
 
@@ -16,10 +16,32 @@ class RMA(Blame):
     # Here, we need a reference to a refundline in order to handle RMAs fully internally
 
 
-class RMAState(ImmutableBlame):
+class InternalRMA(Blame):
 
-    RMA_STATES = ('U', 'N', 'W', 'R', 'S', 'P', 'F', 'A', 'C')
+    general_rma = models.ForeignKey(RMA)
+
+    state = models.CharField(max_length=3)
+
+
+class CustomerRMA(Blame):
+
+    general_rma = models.ForeignKey(RMA)
+
+    state = models.CharField(max_length=3)
+
+
+class InternalRMAState(ImmutableBlame):
+
+    RMA_STATES = ('U', 'N', 'W', 'R', 'S', 'P', 'F', 'A')
+    OPEN_STATES = ('U', 'W', 'R', 'S')
+    CLOSED_STATES = ('N', 'P', 'F', 'A')
     RMA_STATE_EXPLANATIONS = {'U': 'Untested', 'N': 'Not broken', 'W': 'Waiting for Number', 'R': 'Received Number',
-                              'S': 'Sent', 'P': 'Replaced', 'F': 'Refunded by External', 'A': 'Abused by Customer',
-                              'C': 'Returned to Customer'}
+                              'S': 'Sent', 'P': 'Replaced', 'F': 'Refunded by External', 'A': 'Abused by Customer'}
+
+
+class CustomerRMAState(ImmutableBlame):
+    RMA_STATES = ('O', 'L', 'E', 'D')
+    OPEN_STATES = ('E')
+    CLOSED_STATES = ('O', 'L', 'D')
+    RMA_STATE_EXPLANATIONS = {'E': 'Unresolved', 'D': 'Refunded', 'L': 'Replacement product', 'O': 'Returned old product'}
 
