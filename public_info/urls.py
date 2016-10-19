9c99b3d86809f63aac844637e7cb6aa38eddb7ea
mattashii <boekewurm@gmail.com>
2016-10-19 21:11:41 +0200

    updatet urls, now you can actually reverse url the shared object.

--- public_info/urls.py
+++ public_info/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 
 
-
 app_name = 'urlshare'
 
 urlpatterns = (
-    url(r'share/(P<random_str>[a-zA-Z0-9\+-]{16})/', None, name='shared')
+    url(r'(P<random_str>[a-zA-Z0-9\+-]{16})/', None, name='shared')
 )
