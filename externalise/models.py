c2741f494639bb163f701ca858dbe7c5fdeaa703
Léon van der Kaap <leonkaap@gmail.com>
2016-11-02 02:27:22 +0100

    Some updates on the models. Ready for testing.

--- externalise/models.py
+++ externalise/models.py
@@ -1,10 +1,13 @@
-from django.db import models
+from django.db import models, transaction
 from blame.models import Blame, ImmutableBlame
 from article.models import ArticleType
-from money.models import CostField
+from money.models import CostField, Cost
 from crm.models import User
 from decimal import Decimal
-
+from stock.models import StockChangeSet
+from stock.enumeration import enum
+from collections import defaultdict
+from tools.util import raiseif
 
 class ExternaliseDocument(Blame):
     """
@@ -16,14 +19,44 @@ class ExternaliseDocument(Blame):
     memo = models.TextField()
 
     @staticmethod
-    def create_external_products_document(user: User, article_information_list):
+    @transaction.atomic
+    def create_external_products_document(user: User, article_information_list, memo: str):
         """
 
         :param user: The user that created the document
         :param article_information_list: List(Tuple(article, count, cost)). A list of articles with multiplicity and cost.
         This is enough info to create the stock.
         """
-        pass
+        raiseif(not isinstance(user, User), IncorrectClassError)
+        raiseif(not isinstance(memo, str), IncorrectClassError)
+        counter = defaultdict(lambda: 0)
+        for article, count, cost in article_information_list:
+            raiseif(not isinstance(article, ArticleType), IncorrectClassError)
+            raiseif(not isinstance(count, int), IncorrectClassError)
+            raiseif(not isinstance(cost, Cost), IncorrectClassError)
+            raiseif(count <= 0, IncorrectCountError("Count is less than or equal to 0. This is not possible"))
+            raiseif(cost._amount < Decimal(0), IncorrectPriceError("Products cost negative amount of money. This cannot happen."))
+            counter[(article, cost)] += count
+
+        stock_entries = []
+        ext_lines = []
+        for article, cost in counter.keys():
+            stock_entries.append({'article': article,
+                                  'book_value': cost,
+                                  'count': counter[(article, cost)],
+                                  'is_in': True})
+            ext_lines.append(ExternaliseLine(article_type=article, count=counter[(article, cost)], cost=cost))
+
+        # We constructed what we needed, now the saves
+
+        doc = ExternaliseDocument(memo=memo, user_modified=user)
+        doc.save()
+        StockChangeSet.construct(description="Externalisation by document {}".format(doc.pk), entries=stock_entries,
+                                 enum=enum['externalise'])
+        for line in ext_lines:
+            line.externalise_document = doc
+            line.user_modified = user
+            line.save()
 
 
 class ExternaliseLine(ImmutableBlame):
@@ -31,22 +64,44 @@ class ExternaliseLine(ImmutableBlame):
     The line that contains the history information. The products itself will enter stock.
     """
 
+    externalise_document = models.ForeignKey(ExternaliseDocument)
+
     article_type = models.ForeignKey(ArticleType)
 
     count = models.IntegerField()
 
     cost = CostField()
 
-    def save(self, **kwargs):
+    def save(self, mod_stock=True, **kwargs):
         if not self.pk:
-            if self.count > 0 and self.cost.amount >= Decimal(0):
-                pass
-            else:
-                raise IncorrectStockDataError("Either stock is non-positive "
-                                              "or cost is non-positive. Count: {}, Cost: {}".format(self.count, self.cost.amount))
+            if mod_stock:
+                if self.count > 0 and self.cost.amount >= Decimal(0):
+                    entry = [
+                        {'article': self.article_type,
+                         'book_value': self.cost,
+                         'count': self.count,
+                         'is_in': True}
+                    ]
+                    StockChangeSet.construct(description="Direct externalisation from new externaliseline", entries=entry,
+                                             enum=enum['externalise'])
+                else:
+                    raise IncorrectStockDataError("Either stock is non-positive "
+                                                  "or cost is non-positive. Count: {}, Cost: {}".format(self.count, self.cost.amount))
         else:
             super(ExternaliseLine, self).save()
 
 
 class IncorrectStockDataError(Exception):
+    pass
+
+
+class IncorrectCountError(Exception):
+    pass
+
+
+class IncorrectPriceError(Exception):
+    pass
+
+
+class IncorrectClassError(Exception):
     pass
