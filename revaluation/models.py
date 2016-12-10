4b18858ad9c9da142339d0545146a1e1af4c5266
Léon van der Kaap <leonkaap@gmail.com>
2016-12-10 23:58:43 +0100

    Finished functions that revalue the stock and wrote tests.
    - Finished the last of the models and made revaluation document mutable
    - Constructed the functions that revalue
    - Reran the migrations
    - Wrote a bunch of tests
    - Added an extra element to the stock label enums
    - Added some extra fields to the mixins

--- revaluation/models.py
+++ revaluation/models.py
@@ -4,11 +4,12 @@ from article.models import ArticleType
 from money.models import CostField, Cost
 from crm.models import User
 from tools.util import raiseif, raiseifnot
-from stock.models import Stock
+from stock.models import Stock, StockChangeSet
 from stock.stocklabel import StockLabel
+from stock.enumeration import enum
 
 
-class RevaluationDocument(ImmutableBlame):
+class RevaluationDocument(Blame):
     """
     A document for revaluation. Contains lines with revaluation
     """
@@ -16,51 +17,91 @@ class RevaluationDocument(ImmutableBlame):
     memo = models.CharField(max_length=255)
 
     @staticmethod
-    def create_revaluation_document(user: User, article_type_cost_label_combinations):
+    def create_revaluation_document(user: User, article_type_cost_label_combinations, memo: str):
         """
         Revalues a stockLine from its current price to a new price. Can be done to any line in the stock although
         it seems like that it will be done mostly for general unlabeled stockLines.
         :param user: The user that created the revaluation
         :param article_type_cost_label_combinations: List[Tuple[articleType, cost, labelType, labelKey]].
+        :param memo: A memo with the reason for revaluation
         Contains all the information to re-valuate. 'articleType' is the article from a stockLine that should be revalued
-        'cost' is the new cost. In case of stock, both labelType and
-        labelKey should be 'None'
+        'cost' is the new cost. In case of stock, both labelType
+        labelKey should be 'None'. It is highly advised to have the currency of the new cost be the system currency
+        as correct behavior can only be guaranteed if this is the case.
         """
         raiseif(not isinstance(user, User), TypeError)
         revaluation_lines = []
+        stock_mods = []
         for article, cost, label_type, label_key in article_type_cost_label_combinations:
             raiseifnot(isinstance(article, ArticleType), TypeError, "article should be ArticleType")
             raiseifnot(isinstance(cost, Cost), TypeError, "cost should be Cost")
-            raiseifnot(isinstance(label_type, StockLabel) or label_type is None, TypeError, "labelType should be StockLabel")
+            raiseif(cost._amount<0, CostError, "New cost is negative value. This is not possible")
+            raiseifnot(label_type is None or issubclass(label_type, StockLabel), TypeError, "labelType should be StockLabel")
             raiseifnot(isinstance(label_key, int) or label_key is None, TypeError, "labelKey should be int")
-            sts = Stock.objects.filter(article=article, labeltype=label_type, labelkey=label_key)
+            raiseifnot(isinstance(memo, str), TypeError, "Memo should be a string")
+            if label_type is None:
+                sts = Stock.objects.filter(article=article, labeltype=None, labelkey=None)
+            else:
+                sts = Stock.objects.filter(article=article, labeltype=label_type._labeltype, labelkey=label_key)
+
             if len(sts) > 1:
                 raise FatalStockException(article, label_type, label_key, "If you read this, Stock probably broke "
                                                                           "horribly.")
             elif len(sts) == 0:
                 raise NoStockExistsError("Cannot revalue since the article label combination does not exist")
             else:
-                pass
+                stock_line = sts[0]  # type: Stock
+                count = stock_line.count
+                former_cost = stock_line.book_value
+                revaluation_lines.append(RevaluationLine(article_type=article, former_cost=former_cost,
+                                                         new_cost=cost, count=count))
+                if label_type:
+                    stock_mods.append({"article": article,
+                                       "book_value": stock_line.book_value,
+                                       "count": stock_line.count,
+                                       "is_in": False,
+                                       "label": label_type(label_key)})
+                    stock_mods.append({"article": article,
+                                       "book_value": cost,
+                                       "count": stock_line.count,
+                                       "is_in": True,
+                                       "label": label_type(label_key)})
 
+                else:
+                    stock_mods.append({"article": article,
+                                       "book_value": stock_line.book_value,
+                                       "count": stock_line.count,
+                                       "is_in": False})
+                    stock_mods.append({"article": article,
+                                       "book_value": cost,
+                                       "count": stock_line.count,
+                                       "is_in": True})
 
+        # Everything here should be ok, we can save here
+        doc = RevaluationDocument(memo=memo, user_modified=user)
+        doc.save()
+        for line in revaluation_lines:  # type: RevaluationLine
+            line.revaluation_document = doc
+            line.user_modified = user
+            line.save()
 
-
-
-
+        StockChangeSet.construct(description="Revaluation document {}".format(doc.id), entries=stock_mods,
+                                 enum=enum["revaluation"])
 
     @staticmethod
-    def create_revaluation_document_stock(user: User, article_type_cost_combination):
+    def create_revaluation_document_stock(user: User, article_type_cost_combination, memo: str):
         """
         Has the same functionality as 'create_revaluation_document' but just for stock.
         :param user: The user that created the revaluation
         :param article_type_cost_combination: List[Tuple[articleType, cost]]. 'articleType' is the article from a stockLine that should be revalued
         'cost' is the new cost.
+        :param memo: A memo with the reason for revaluation
         """
         expanded_lines = []
         for article_type, cost in article_type_cost_combination:
             expanded_lines.append((article_type, cost, None, None))
 
-        return RevaluationDocument.create_revaluation_document(user, expanded_lines)
+        return RevaluationDocument.create_revaluation_document(user, expanded_lines, memo)
 
 
 class RevaluationLine(Blame):
@@ -96,3 +137,5 @@ class FatalStockException(Exception):
         return self.text
 
 
+class CostError(Exception):
+    pass
--- revaluation/tests.py
+++ revaluation/tests.py
@@ -1,12 +1,192 @@
 from django.test import TestCase
 from tools.testing import TestData
-from revaluation.models import RevaluationDocument
+from revaluation.models import RevaluationDocument, NoStockExistsError, RevaluationLine, CostError
+from stock.models import StockChangeSet, Stock
+from stock.stocklabel import OrderLabel
+from money.models import Cost
+from decimal import Decimal
+
 
 class RevaluationTests(TestCase, TestData):
 
     def setUp(self):
         self.setup_base_data()
 
-    def test_fake_get_stock(self):
-        RevaluationDocument.create_revaluation_document(self.user_1, [(self.articletype_1, self.cost_eur_1, None, None)])
+    def test_revaluation_of_non_existent_general_stock(self):
+        stock_addition = []
+        stock_addition.append({"article": self.articletype_2,
+                               "book_value": self.cost_eur_1,
+                               "count": 5,
+                               "is_in": True})
+        StockChangeSet.construct(description="Test", entries=stock_addition, enum=0)
+        with self.assertRaises(NoStockExistsError):
+            RevaluationDocument.create_revaluation_document(user=self.user_1,article_type_cost_label_combinations=[(self.articletype_1, self.cost_eur_1, None, None)], memo="")
+
+    def test_revaluation_of_wrong_label_stock(self):
+        stock_addition = []
+        stock_addition.append({"article": self.articletype_2,
+                               "book_value": self.cost_eur_1,
+                               "count": 5,
+                               "is_in": True,
+                               "label": OrderLabel(1)})
+        StockChangeSet.construct(description="Test", entries=stock_addition, enum=0)
+        with self.assertRaises(NoStockExistsError):
+            RevaluationDocument.create_revaluation_document(user=self.user_1, article_type_cost_label_combinations=[
+                (self.articletype_1, self.cost_eur_1, None, None)], memo="")
+
+    def test_revaluation_single_general_stock_line(self):
+        stock_addition = []
+        COUNT = 5
+        stock_addition.append({"article": self.articletype_1,
+                               "book_value": self.cost_eur_1,
+                               "count": COUNT,
+                               "is_in": True})
+        StockChangeSet.construct(description="Test", entries=stock_addition, enum=0)
+        RevaluationDocument.create_revaluation_document(user=self.user_1,
+                                                        article_type_cost_label_combinations=[(self.articletype_1, self.cost_eur_2, None, None)],
+                                                        memo="")
+        reval_doc = RevaluationDocument.objects.get()
+        reval_line = RevaluationLine.objects.get()
+        self.assertEqual(reval_line.revaluation_document, reval_doc)
+        self.assertEqual(reval_line.former_cost, self.cost_eur_1)
+        self.assertEqual(reval_line.new_cost, self.cost_eur_2)
+        self.assertEqual(reval_line.count, COUNT)
+        self.assertEqual(reval_line.article_type, self.articletype_1)
+
+    def test_revaluation_of_labeled_stock(self):
+        stock_addition = []
+        COUNT = 5
+        stock_addition.append({"article": self.articletype_1,
+                               "book_value": self.cost_eur_1,
+                               "count": COUNT,
+                               "is_in": True,
+                               "label": OrderLabel(1)})
+        StockChangeSet.construct(description="Test", entries=stock_addition, enum=0)
+        RevaluationDocument.create_revaluation_document(user=self.user_1,
+                                                        article_type_cost_label_combinations=[
+                                                            (self.articletype_1, self.cost_eur_2, OrderLabel, 1)],
+                                                        memo="")
+        reval_doc = RevaluationDocument.objects.get()
+        reval_line = RevaluationLine.objects.get()
+        self.assertEqual(reval_line.revaluation_document, reval_doc)
+        self.assertEqual(reval_line.former_cost, self.cost_eur_1)
+        self.assertEqual(reval_line.new_cost, self.cost_eur_2)
+        self.assertEqual(reval_line.count, COUNT)
+        self.assertEqual(reval_line.article_type, self.articletype_1)
+
+    def test_revaluation_multiple_stocks(self):
+        stock_addition = []
+        COUNT_1 = 5
+        COUNT_2 = 3
+        stock_addition.append({"article": self.articletype_1,
+                               "book_value": self.cost_eur_1,
+                               "count": COUNT_1,
+                               "is_in": True,
+                               })
+        stock_addition.append({"article": self.articletype_2,
+                               "book_value": self.cost_eur_3,
+                               "count": COUNT_2,
+                               "is_in": True,
+                               })
+        StockChangeSet.construct(description="Test", entries=stock_addition, enum=0)
+        RevaluationDocument.create_revaluation_document(user=self.user_1,
+                                                        article_type_cost_label_combinations=[
+                                                            (self.articletype_1, self.cost_eur_2, None, None),
+                                                            (self.articletype_2, self.cost_eur_4, None, None)],
+                                                        memo="")
+        reval_doc = RevaluationDocument.objects.get()
+        reval_line = RevaluationLine.objects.all()
+        self.assertEqual(len(reval_line), 2)
+        if reval_line[0].article_type == self.articletype_1:
+            reval_1 = reval_line[0]
+            reval_2 = reval_line[1]
+        else:
+            reval_1 = reval_line[1]
+            reval_2 = reval_line[0]
+        self.assertEqual(reval_1.revaluation_document, reval_doc)
+        self.assertEqual(reval_1.former_cost, self.cost_eur_1)
+        self.assertEqual(reval_1.new_cost, self.cost_eur_2)
+        self.assertEqual(reval_1.count, COUNT_1)
+        self.assertEqual(reval_1.article_type, self.articletype_1)
+
+        self.assertEqual(reval_2.revaluation_document, reval_doc)
+        self.assertEqual(reval_2.former_cost, self.cost_eur_3)
+        self.assertEqual(reval_2.new_cost, self.cost_eur_4)
+        self.assertEqual(reval_2.count, COUNT_2)
+        self.assertEqual(reval_2.article_type, self.articletype_2)
+
+    def test_shorthand_stock_method(self):
+        stock_addition = []
+        COUNT_1 = 5
+        COUNT_2 = 3
+        stock_addition.append({"article": self.articletype_1,
+                               "book_value": self.cost_eur_1,
+                               "count": COUNT_1,
+                               "is_in": True,
+                               })
+        stock_addition.append({"article": self.articletype_2,
+                               "book_value": self.cost_eur_3,
+                               "count": COUNT_2,
+                               "is_in": True,
+                               })
+        StockChangeSet.construct(description="Test", entries=stock_addition, enum=0)
+        RevaluationDocument.create_revaluation_document_stock(user=self.user_1,
+                                                              article_type_cost_combination=[
+                                                            (self.articletype_1, self.cost_eur_2)
+                                                            ],
+                                                        memo="")
+
+        reval_doc = RevaluationDocument.objects.get()
+        reval_line = RevaluationLine.objects.get()
+        self.assertEqual(reval_line.revaluation_document, reval_doc)
+        self.assertEqual(reval_line.former_cost, self.cost_eur_1)
+        self.assertEqual(reval_line.new_cost, self.cost_eur_2)
+        self.assertEqual(reval_line.count, COUNT_1)
+        self.assertEqual(reval_line.article_type, self.articletype_1)
+
+    def test_negative_price(self):
+        stock_addition = []
+        stock_addition.append({"article": self.articletype_2,
+                               "book_value": self.cost_eur_1,
+                               "count": 5,
+                               "is_in": True})
+        StockChangeSet.construct(description="Test", entries=stock_addition, enum=0)
+        with self.assertRaises(CostError):
+            RevaluationDocument.create_revaluation_document(user=self.user_1, article_type_cost_label_combinations=[
+                (self.articletype_1, Cost(amount=Decimal(-3.14), currency=self.currency_eur), None, None)], memo="")
+
+    def test_one_article_multiple_labels(self):
+        stock_addition = []
+        COUNT_1 = 5
+        COUNT_2 = 3
+        stock_addition.append({"article": self.articletype_1,
+                               "book_value": self.cost_eur_1,
+                               "count": COUNT_1,
+                               "is_in": True,
+                               })
+        stock_addition.append({"article": self.articletype_1,
+                               "book_value": self.cost_eur_3,
+                               "count": COUNT_2,
+                               "is_in": True,
+                               "label": OrderLabel(1)
+                               })
+        StockChangeSet.construct(description="Test", entries=stock_addition, enum=0)
+        RevaluationDocument.create_revaluation_document(user=self.user_1,
+                                                        article_type_cost_label_combinations=[
+                                                            (self.articletype_1, self.cost_eur_2, None, None),
+                                                            ],
+                                                        memo="")
+        reval_doc = RevaluationDocument.objects.get()
+        reval_line = RevaluationLine.objects.get()
+        self.assertEqual(reval_line.revaluation_document, reval_doc)
+        self.assertEqual(reval_line.former_cost, self.cost_eur_1)
+        self.assertEqual(reval_line.new_cost, self.cost_eur_2)
+        self.assertEqual(reval_line.count, COUNT_1)
+        self.assertEqual(reval_line.article_type, self.articletype_1)
+        # Assure no change in labeled stock
+        st = Stock.objects.get(article=self.articletype_1, labeltype=OrderLabel._labeltype)
+        self.assertEqual(st.book_value, self.cost_eur_3)
+
+
+
 
