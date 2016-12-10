bd66ba5e72d4fef3d4a5c5279b8ea54e49d53232
mattashii <boekewurm@gmail.com>
2016-12-10 04:38:24 +0100

    Add API get/post-functions. This means that you don't need to worry about headers/baseURL anymore, just give it an address and, in the case of POST, an JSON.stringify()-able object. Last argument can be an object with overrides for the fetch used.

---
+++ frontend/src/api.js
@@ -0,0 +1,34 @@
+import fetch from "isomorphic-fetch";
+import config from "./config";
+import {getState} from "./app";
+
+export function get(url, {headers = {}, ...rest}) {
+	return fetch(
+		config.baseurl + url,
+		{
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Token ${getState().auth.token}`,
+				...headers,
+			},
+			...rest,
+		}
+	).then((response) => response.ok ? response : Promise.reject(response))
+}
+
+export function post(url, object, {headers = {}, ...rest}) {
+	return fetch(
+		config.baseurl + url,
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Token ${getState().auth.token}`,
+				...headers,
+			},
+			body: JSON.stringify(object),
+			...rest,
+		}
+	).then((response) => response.ok ? response : Promise.reject(response))
+}
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -26,6 +26,8 @@ const store = createStore(
 	composeEnhancers(applyMiddleware(routerMiddleware(browserHistory), sagaMiddleware))
 );
 
+export const getState = store.getState.bind(store);
+
 // Run the main saga
 sagaMiddleware.run(saga);
 
