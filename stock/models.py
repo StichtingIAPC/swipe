383457f2a3e795a146d17bae53b2c70e316eea5e
Kevin Alberts <kevin@kevinalberts.nl>
2017-01-05 22:36:03 +0100

    stock: Fix build error: name '_' is not defined

--- stock/models.py
+++ stock/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db import transaction
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 from article.models import ArticleType
 from money.models import CostField
