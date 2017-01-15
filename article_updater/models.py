0f793b622cc5bcb5a4257da1d99bb13e81538f3c
Léon van der Kaap <leonkaap@gmail.com>
2017-01-15 20:01:40 +0100

    Rework of the parsers, used internal CSV-parser. Added some tests. Added helper file to speed up testing.

--- article_updater/models.py
+++ article_updater/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from supplier.models import Supplier
 from tools.util import raiseifnot
 
+# For file lookup
 import os.path
+# For seeing that a file is plaintext-ish
 import mimetypes
 
 import xml.etree.ElementTree as ET
@@ -11,48 +13,38 @@ class FileParser:
 
     @staticmethod
     def verify_file_exists(file_location: str) -> bool:
+        """
+        Checks is a file exists
+        :param file_location: A file path
+        :return: The file at the file path exists.
+        """
         raiseifnot(isinstance(file_location, str), TypeError)
         return os.path.isfile(file_location)
 
     @staticmethod
     def verify_file_is_plain_text(file_location: str) -> bool:
+        """
+        Contrary to the name, this function cannot actually check if a file is a plain text file.
+        This function checks if it is likely that this file is parsable in a plain text manner.
+        The intended purpose is to check if it can be parsed as a csv or xml type. The most reliable
+        manner to do this is to use mimetypes. Unfortunately, there are several different mimetypes
+        that may or not be plain text. The chosen algorithm therefore errors on the side of
+        permissiveness and indicates that it's likely that a file is plain text. This function should
+        be edited if there are more relevant documents that are plain text but which the function refuses
+        to accept.
+        :param file_location: A file path
+        :return: The file at the path is probably plain text.
+        """
         raiseifnot(isinstance(file_location, str), TypeError)
         mimetype = mimetypes.guess_type(file_location)[0]  # type: str
         mimetype_first_part = mimetype.split('/')[0]
-        return mimetype_first_part == 'text' or mimetype == 'application/xml'
-
-
-class SupplierDataParser:
-
-    @staticmethod
-    def parse(file_location):
-        pass
-
-
-class XMLParser(SupplierDataParser):
-
-    @staticmethod
-    def parse(file_location):
-        raiseifnot(FileParser.verify_file_exists(file_location) and FileParser.verify_file_is_plain_text(file_location),
-                   ParseError, "File is not an existing plain text file")
-        try:
-            result = ET.parse(file_location)
-            return result
-        except ET.ParseError:
-            raise ParseError("File is not a valid XML file")
-
-
-class CSVParser(SupplierDataParser):
-
-    @staticmethod
-    def parse(file_location):
-        raiseifnot(FileParser.verify_file_exists(file_location) and FileParser.verify_file_is_plain_text(file_location),
-                   ParseError, "File is not an existing plain text file")
-
+        possible_correct_mimes = ['application/vnd.ms-excel', 'application/xml']
+        return mimetype_first_part == 'text' or mimetype in possible_correct_mimes
 
 class DataTypeSupplierRelation(models.Model):
 
     supplier = models.ForeignKey(Supplier)
+
 
 class XMLSupplierRelation(DataTypeSupplierRelation):
 
@@ -80,7 +72,7 @@ class CSVSupplierRelation(DataTypeSupplierRelation):
 
     # The element separator
     separator = models.CharField(max_length=5)
-    # The first line(s) can be CSV-metadata
+    # The first line(s) can be CSV-metadata. The first line of a file is considered to be line 0.
     start_at = models.IntegerField()
     # Unique product identifier for supplier. Unique referer per supplier.
     number = models.IntegerField()
@@ -98,5 +90,45 @@ class CSVSupplierRelation(DataTypeSupplierRelation):
     packing_amount = models.IntegerField()
 
 
-class ParseError(Exception):
+class SupplierDataParser:
+    pass
+
+
+class XMLParser(SupplierDataParser):
+
+    @staticmethod
+    def parse(file_location, supplier_data: XMLSupplierRelation) -> ET.ElementTree:
+        raiseifnot(FileParser.verify_file_exists(file_location) and FileParser.verify_file_is_plain_text(file_location),
+                   SwipeParseError, "File is not an existing plain text file")
+        # Can throw a ParseError
+        result = ET.parse(file_location)
+        return result
+
+
+class CSVParser(SupplierDataParser):
+
+    @staticmethod
+    def parse(file_location, supplier_data: CSVSupplierRelation):
+        raiseifnot(FileParser.verify_file_exists(file_location) and FileParser.verify_file_is_plain_text(file_location),
+                   SwipeParseError, "File is not an existing plain text file")
+        return CSVParser.parse_csv(file_location, supplier_data)
+
+    @staticmethod
+    def parse_csv(file_location, supplier_data: CSVSupplierRelation):
+        if not FileParser.verify_file_exists(file_location) or not FileParser.verify_file_is_plain_text(file_location) \
+                or mimetypes.guess_type(file_location)[0] in ['text/xml', 'application/xml']:
+                    raise SwipeParseError("File is not a CSV file")
+        file = open(file_location, 'r', encoding='utf-8')
+        lines = file.readlines()
+        element_lines = []
+        for i in range(supplier_data.start_at, len(lines)):
+            elements = lines[i].split(supplier_data.separator)
+            for j in range(len(elements)):
+                if elements[j].startswith('"') and elements[j].endswith('"'):
+                    elements[j] = elements[j][1:-1]
+            element_lines.append(elements)
+        return element_lines
+
+
+class SwipeParseError(Exception):
     pass
--- article_updater/tests.py
+++ article_updater/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, SimpleTestCase
-from article_updater.models import FileParser
+from xml.etree.ElementTree import ParseError
+from article_updater.models import FileParser, XMLParser, CSVParser, SwipeParseError, CSVSupplierRelation
 import mimetypes
 
 
@@ -11,16 +12,43 @@ class ParserTests(SimpleTestCase):
     def test_file_location_does_not_exist(self):
         self.assertFalse(FileParser.verify_file_exists("./article_updater/ulaan_bator.py"))
 
-    # This should work but doesn't. Finding out why might be difficult due to different
-    # computers giving different answers.
     def test_xml_file_is_plain_text(self):
         file_location = "./article_updater/testing/Copaco_prijslijst_91658.xml"
         self.assertTrue(FileParser.verify_file_is_plain_text(file_location), mimetypes.guess_type(file_location)[0])
 
     def test_csv_file_is_plain_text(self):
-        pass
+        file_location = "./article_updater/testing/nedis_csv_full.csv"
+        self.assertTrue(FileParser.verify_file_is_plain_text(file_location), mimetypes.guess_type(file_location)[0])
 
     def test_file_is_not_plain_text(self):
         # I see what I did there
         self.assertFalse(FileParser.verify_file_is_plain_text("./article_updater/testing/PB145906.jpg"))
+
+    @staticmethod
+    def test_parsing_xml_does_not_error():
+        file_location = "./article_updater/testing/Copaco_prijslijst_91658-brief.xml"
+        XMLParser.parse(file_location, None)
+
+    def test_parsing_xml_from_csv_does_error(self):
+        file_location = "./article_updater/testing/nedis_csv_brief.csv"
+        with self.assertRaises(ParseError):
+            XMLParser.parse(file_location, None)
+
+    @staticmethod
+    def test_parsing_csv_does_not_error():
+        file_location = "./article_updater/testing/nedis_csv_brief.csv"
+        result = CSVParser.parse(file_location, CSVSupplierRelation(separator=",", start_at=1))
+
+    def test_parsing_csv_from_xml_does_error(self):
+        file_location = "./article_updater/testing/Copaco_prijslijst_91658-brief.xml"
+        with self.assertRaises(SwipeParseError):
+            result = CSVParser.parse(file_location, CSVSupplierRelation(separator=",", start_at=1))
+
+    def test_parsing_headless_file(self):
+        file_location = "./article_updater/testing/nedis_csv_brief.csv"
+        result = CSVParser.parse(file_location, CSVSupplierRelation(separator=",", start_at=1))
+        self.assertEqual(3, len(result))
+        self.assertEqual("CMP-CE039-1.5", result[0][2])
+        self.assertEqual("CMP-CE070", result[1][2])
+        self.assertEqual("CMP-CE070/3", result[2][2])
 
