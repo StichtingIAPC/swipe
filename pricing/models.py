b66dd56fedd6635ea2ea65d0408c36a12094dac6
Léon van der Kaap <leonkaap@gmail.com>
2017-01-26 20:59:51 +0100

    First data and tests in pricing.

--- pricing/models.py
+++ pricing/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+def validate_bigger_than_0(value):
+    if value < 1:
+        raise ValidationError("Value of pricingmodel should be bigger than 0")
+
+
+class PricingModel(models.Model):
+    # A name indicates what the method does, not functional.
+    name = models.CharField(max_length=40)
+    # The priority of execution of the function. Lower number is higher priority. Number is bigger than 0 and unique.
+    position = models.IntegerField(unique=True, null=False, validators=[validate_bigger_than_0])
+
+    def __str__(self):
+        return "Id: {}, Name: {}, Position: {}".format(self.id, self.name, self.position)
+    
+    def save(self):
+        validate_bigger_than_0(self.position)
+        super(PricingModel, self).save()
+
--- pricing/tests.py
+++ pricing/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
+from tools.testing import TestData
+from pricing.models import PricingModel
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+
+class PriorityTests(TestCase, TestData):
+
+    def setUp(self):
+        self.setup_base_data()
+
+    def test_uniqueness(self):
+        p1 = PricingModel(name="Foo", position=1)
+        p2 = PricingModel(name="Bar", position=1)
+        p1.save()
+        with self.assertRaises(IntegrityError):
+            p2.save()
+
+    def test_position_bigger_than_0(self):
+        p1 = PricingModel(name="Foo", position=-1)
+        with self.assertRaises(ValidationError):
+            p1.save()
