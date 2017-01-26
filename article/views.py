a889853e95b03ec5c95f2cffe26edd3fcb47951c
mattashii <boekewurm@gmail.com>
2017-01-26 22:13:21 +0100

    Fix serialization, add MoneyFieldSerializer, and updatet some style

--- article/views.py
+++ article/views.py
@@ -9,26 +9,24 @@ from article.serializers import ArticleTypeSerializer
 class ArticleListView(mixins.ListModelMixin,
                       mixins.CreateModelMixin,
                       generics.GenericAPIView):
-
     queryset = ArticleType.objects.all()
     serializer_class = ArticleTypeSerializer
 
     def get(self, request, *args, **kwargs):
-        return self.list(request=request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        return self.create(request=request, *args, **kwargs)
+        return self.create(request, *args, **kwargs)
 
 
 class ArticleTypeView(mixins.RetrieveModelMixin,
                       mixins.UpdateModelMixin,
                       generics.GenericAPIView):
-
     queryset = ArticleType.objects.all()
     serializer_class = ArticleTypeSerializer
 
     def get(self, request, *args, **kwargs):
-        return self.retrieve(request=request, *args, **kwargs)
+        return self.retrieve(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
-        return self.update(request=request, *args, **kwargs)
+        return self.update(request, *args, **kwargs)
