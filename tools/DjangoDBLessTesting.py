c491c80399b4050a207e1b84fd115e0f0544dd89
Léon van der Kaap <leonkaap@gmail.com>
2017-01-26 21:24:16 +0100

    Comment for DB-less testing.

--- tools/DjangoDBLessTesting.py
+++ tools/DjangoDBLessTesting.py
@@ -2,7 +2,9 @@ from django.test.runner import DiscoverRunner
 
 
 class NoDbTestRunner(DiscoverRunner):
-    """ A test runner to test without database creation """
+    """ A test runner to test without database creation. This can be used when doing tests that do not communicate
+     with the database. Select this particular class as runner (tools.DjangoDBLessTesting.NoDbTestRunner) when you want
+     to skip waiting for DB migration."""
 
     def setup_databases(self, **kwargs):
         """ Override the database creation defined in parent class """
