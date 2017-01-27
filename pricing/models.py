e2f2641e51088dab2e99f0adcf18baed22059fbb
Léon van der Kaap <leonkaap@gmail.com>
2017-01-27 20:30:08 +0100

    Simple price returning functionality.

--- pricing/models.py
+++ pricing/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from article.models import SellableType
+from tools.util import raiseifnot
 
 
 def validate_bigger_than_0(value):
@@ -8,6 +10,11 @@ def validate_bigger_than_0(value):
 
 
 class PricingModel(models.Model):
+    """
+    This is a translater from row to actual processing function
+    """
+    # The primary key, also the key value reference to the function
+    function_identifier = models.IntegerField(primary_key=True)
     # A name indicates what the method does, not functional.
     name = models.CharField(max_length=40)
     # The priority of execution of the function. Lower number is higher priority. Number is bigger than 0 and unique.
@@ -20,3 +27,48 @@ class PricingModel(models.Model):
         validate_bigger_than_0(self.position)
         super(PricingModel, self).save()
 
+    def return_pricing_function(self):
+        """
+        Retrieves a pricing function from its unique identifier
+        :return:
+        """
+        function_dict = {1: Functions.fixed_price}
+
+        return function_dict.get(self.id)
+
+    @staticmethod
+    def return_price(sellable_type: SellableType=None):
+        pricing_models = PricingModel.objects.all().order_by('function_identifier')
+        if len(pricing_models) == 0:
+            raise PricingError("No pricing models found!")
+        else:
+            price = None
+            i=0
+            while price is None and i < len(pricing_models):
+                pricing_function = pricing_models[i].return_pricing_function()
+                price = pricing_function(sellable_type=sellable_type)
+                i+=1
+
+            if not price:
+                raise PricingError("Pricing models were applied and nothing appropriate was found. No price returned.")
+
+            return price
+
+
+class Functions:
+    """
+    A container class for all the pricing functions. All function should have the same arguments to accomodate
+    seamless insertion of new pricing functions.
+    """
+
+    @staticmethod
+    def fixed_price(sellable_type: SellableType=None):
+        raiseifnot(isinstance(sellable_type, SellableType), TypeError, "sellableType should be sellableType")
+        if hasattr(sellable_type, 'fixed_price'):
+            return sellable_type.fixed_price
+        else:
+            return None
+
+
+class PricingError(Exception):
+    pass
