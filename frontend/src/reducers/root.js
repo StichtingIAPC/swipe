fe647be29a8aa35983870c977c7098114aab3010
mattashii <boekewurm@gmail.com>
2017-02-20 23:47:29 +0100

    create articles saga

--- frontend/src/reducers/root.js
+++ frontend/src/reducers/root.js
@@ -1,11 +1,13 @@
-import {combineReducers} from "redux";
-import {routerReducer} from "react-router-redux";
+import { combineReducers } from "redux";
+import { routerReducer } from "react-router-redux";
+import articles from "./articles";
 import suppliers from "./suppliers";
 import auth from "./auth";
 import sidebar from "./sidebar";
 import currencies from "./money/currencies";
 
 export default combineReducers({
+	articles,
 	suppliers,
 	auth,
 	routing: routerReducer,
