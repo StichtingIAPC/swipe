d940ca19e991c7cbe419f686a4ca7a9e3342ea86
Léon van der Kaap <leonkaap@gmail.com>
2017-05-23 02:50:48 +0200

    Standardised the post request and JSON unwrapping.

--- register/views.py
+++ register/views.py
@@ -12,6 +12,47 @@ from register.models import RegisterMaster, Register, DenominationCount, SalesPe
 from register.serializers import RegisterSerializer, PaymentTypeSerializer, \
     RegisterCountSerializer, SalesPeriodSerializer
 
+class DictParsers:
+
+    @staticmethod
+    def decimal_parser(string: str):
+        if string is None:
+            raise ParseError("String does not exist")
+        try:
+            float(string)
+        except ValueError:
+            raise ParseError("String is not a valid decimal")
+        return Decimal(string)
+
+    @staticmethod
+    def string_parser(string: str):
+        if string is None:
+            raise ParseError("String does not exist")
+        return string
+
+    @staticmethod
+    def denominationcount_parser(dictionary: dict):
+        count = dictionary.get("count", None)
+        if count is None:
+            raise ParseError("Count is missing")
+        if not type(count) == int:
+            raise ParseError("Count is not an int")
+        denomination = dictionary.get("denomination", None)
+        if denomination is None:
+            raise ParseError("Denomination is missing")
+        if not type(denomination) == int:
+            raise ParseError("Denomination is not an int")
+        db_denom = Denomination.objects.get(id=denomination)
+        return DenominationCount(denomination=db_denom, number=count)
+
+    @staticmethod
+    def register_parser(integer: int):
+        if integer is None:
+            raise ParseError("Register does not exist")
+        if not type(integer) == int:
+            raise ParseError("Register is not an int")
+        return Register.objects.get(id=integer)
+
 
 class RegisterListView(mixins.ListModelMixin,
                        mixins.CreateModelMixin,
@@ -147,39 +188,41 @@ class RegisterOpenView(mixins.RetrieveModelMixin,
                        generics.GenericAPIView):
     queryset = Register.objects.all()
     serializer_class = RegisterSerializer
-    
+
+    @staticmethod
+    def deconstruct_post_body(body: dict):
+        memo = DictParsers.string_parser(body.get("memo", None))
+        amount = DictParsers.decimal_parser(body.get("amount", None))
+        denoms = body.get("denoms", None)
+        if denoms is None:
+            raise ParseError("Denominationcounts are missing")
+        if not isinstance(denoms, list):
+            raise ParseError("Denominationcounts are malformed")
+        denominations = []
+        for denom in denoms:
+            denominations.append(DictParsers.denominationcount_parser(denom))
+        params = type('', (), {})
+        params.memo = memo
+        params.amount = amount
+        params.denoms = denominations
+        return params
+
     def post(self, request, *args, **kwargs):
         pk = kwargs['pk']
         try:
             register = Register.objects.get(pk=pk)
         except Register.DoesNotExist:
             raise Http404
-        json_data=request.data  #type: dict
-        memo = json_data.get("memo", None)
-        if memo is None:
-            respo = HttpResponseBadRequest(reason="Memo is missing")
-            return respo
-        amount = json_data.get("amount", None) # type: str
-        if amount is None:
-            respo = HttpResponseBadRequest(reason="Amount is missing")
-            return respo
+        json_data = request.data  #type: dict
         try:
-            float(amount)
-        except ValueError:
-            respo = HttpResponseBadRequest(reason="Amount is malformed")
-            return respo
-        counted_amount = Decimal(amount)
-        denoms = json_data.get("denoms", None)
-        if denoms is None:
-            respo = HttpResponseBadRequest(reason="Denominationcounts are missing")
-            return respo
-        if not isinstance(denoms, list):
-            respo = HttpResponseBadRequest(reason="Denominationcounts are malformed")
-            return respo
+            parsed_data = RegisterOpenView.deconstruct_post_body(json_data)
+        except ParseError as e:
+            return HttpResponseBadRequest(reason=str(e))
+
         if not register.is_cash_register:
             # We can ignore the denomination counts and use the counted amount
             try:
-                count = register.open(counted_amount=counted_amount, memo=memo)
+                count = register.open(counted_amount=parsed_data.amount, memo=parsed_data.memo)
                 ser = RegisterCountSerializer().to_representation(count)
                 return HttpResponse(content=json.dumps(ser),
                                     content_type="application/json")
@@ -187,36 +230,10 @@ class RegisterOpenView(mixins.RetrieveModelMixin,
                 respo = HttpResponseBadRequest(reason="Register is already opened")
                 return respo
         else:
-            # We need to get denominations from the database to create our own denomination counts
-            # If the counts don't match, return an error
-            db_denoms = Denomination.objects.filter(currency=register.currency)
-            denom_dict = {}
-            for denom in db_denoms:
-                denom_dict[denom.amount] = denom
-
-            denomination_counts = []
-            for json_denom in denoms:
-                count = json_denom["count"]
-                amount = json_denom["amount"]
-                try:
-                    float(amount)
-                except ValueError:
-                    respo = HttpResponseBadRequest(reason="Denominationcounts are malformed")
-                    return respo
-                if not count.isnumeric():
-                    respo = HttpResponseBadRequest(reason="Denominationcounts are malformed")
-                    return respo
-                decimal_value = Decimal(amount)
-                count_int = int(count)
-                db_denom = denom_dict.get(decimal_value, None)
-                if db_denom is None:
-                    respo = HttpResponseBadRequest(reason="Denominationcounts is of non-existent denomination")
-                    return respo
-                denomination_counts.append(DenominationCount(denomination=db_denom, number=count_int))
-
             try:
-                reg_count = register.open(counted_amount=counted_amount, memo=memo, denominations=denomination_counts)
-                ser=RegisterCountSerializer().to_representation(reg_count)
+                reg_count = register.open(counted_amount=parsed_data.amount,
+                                          memo=parsed_data.memo, denominations=parsed_data.denoms)
+                ser = RegisterCountSerializer().to_representation(reg_count)
                 return HttpResponse(content=json.dumps(ser),
                                     content_type="application/json")
             except AlreadyOpenError:
@@ -240,22 +257,8 @@ class SalesPeriodCloseView(mixins.RetrieveModelMixin,
         counts_and_denom_counts = []
 
         for datum in register_data:
-            register = datum.get("register", None)
-            if register is None:
-                raise ParseError("Register is missing")
-            try:
-                register = int(register)
-            except Exception:
-                raise ParseError("Register is not a number")
-            register = Register.objects.get(id=register)
-            amount = datum.get("amount", None)
-            if amount is None:
-                raise ParseError("Amount is missing")
-            try:
-                float(amount)
-            except Exception:
-                raise ParseError("Amount is not decimal")
-            amount = Decimal(amount)
+            register = DictParsers.register_parser(datum.get("register", None))
+            amount = DictParsers.decimal_parser(datum.get("amount", None))
             reg_count = RegisterCount(register=register, amount=amount)
             denom_counts = []
             denoms = datum.get("denoms", None)
@@ -264,20 +267,9 @@ class SalesPeriodCloseView(mixins.RetrieveModelMixin,
             if not isinstance(denoms, list):
                 raise ParseError("Denoms is not a list")
             for denom in denoms:
-                count = denom.get("count", None)
-                denomination = denom.get("denomination", None)
-                try:
-                    int(denomination)
-                except ValueError:
-                    raise ParseError("Denomination is not a number")
-                db_denom = Denomination.objects.get(id=denomination)
-                try:
-                    count = int(count)
-                except Exception:
-                    raise ParseError("Denominationcounts are malformed")
-                denom_counts.append(DenominationCount(register_count=reg_count,
-                                                      denomination=db_denom,
-                                                      number=count))
+                denom_count = DictParsers.denominationcount_parser(denom)
+                denom_counts.register_count = reg_count
+                denom_counts.append(denom_count)
             counts_and_denom_counts.append((reg_count, denom_counts))
 
         params = type('', (), {})
