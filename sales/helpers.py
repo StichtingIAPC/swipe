8b277cdc5c1b56e5ff823ff4df23856a676f5075
Léon van der Kaap <leonkaap@gmail.com>
2017-06-21 22:04:57 +0200

    Added price overrides.

--- sales/helpers.py
+++ sales/helpers.py
@@ -1,7 +1,8 @@
 from article.helpers import ArticleDictParsers
+from crm.helpers import CRMDictParsers
 from money.helpers import MoneyDictParsers
 from sales.models import Payment, SalesTransactionLine, OtherCostTransactionLine, OtherTransactionLine, \
-    RefundTransactionLine
+    RefundTransactionLine, PriceOverride
 from tools.json_parsers import ParseError, DictParsers
 
 
@@ -18,11 +19,23 @@ class SalesDictParsers:
         return Payment(payment_type_id=payment_type, amount=amount)
 
     @staticmethod
+    def price_override_parser(obj: dict):
+        if obj is None:
+            return None
+        if not isinstance(obj, dict):
+            raise ParseError("PriceOverride is not a dict")
+        user = CRMDictParsers.user_parser(obj.get("user"))
+        reason = DictParsers.string_parser(obj.get("reason"))
+        original_price = MoneyDictParsers.price_parser(obj.get("original_price"))
+        return PriceOverride(user=user, reason=reason, original_price=original_price)
+
+
+    @staticmethod
     def transactionline_parser(obj: dict):
         count = DictParsers.int_parser(obj.get("count"))
         price = MoneyDictParsers.price_parser(obj.get("price"))
         order = DictParsers.int_parser(obj.get("order"), optional=True)
-        original_price = MoneyDictParsers.price_parser(obj.get("original_price"), optional=True)
+        original_price = SalesDictParsers.price_override_parser(obj.get("original_price"))
         clazz = DictParsers.string_parser(obj.get("class"))
         if clazz == "SalesTransactionLine":
             cost = MoneyDictParsers.cost_parser(obj.get("cost"))
