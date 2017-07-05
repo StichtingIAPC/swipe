6b850abbd1299236b8883dd117d1ea8f2ec6d398
MaukWM <maukmul@gmail.com>
2017-07-05 22:30:29 +0200

    added [Update]ArticleTypeSupplierView and edited MoneySerializerField to correctly Serialize a Cost
    and fixed some small issues on frontend

--- article/views.py
+++ article/views.py
@@ -3,6 +3,8 @@ from rest_framework import mixins
 
 from article.models import ArticleType
 from article.serializers import ArticleTypeSerializer
+from supplier.models import ArticleTypeSupplier
+from supplier.serializers import ArticleTypeSupplierSerializer
 from tools.json_parsers import ParseError
 
 
@@ -30,3 +32,31 @@ class ArticleTypeView(mixins.RetrieveModelMixin,
 
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
+
+
+class UpdateArticleTypeSupplierView(mixins.UpdateModelMixin,
+                              generics.GenericAPIView):
+    serializer_class = ArticleTypeSupplierSerializer
+
+    lookup_field = 'supplier'
+
+    def get_queryset(self):
+        return ArticleTypeSupplier.objects.filter(article_type=self.kwargs["article_type"], supplier=self.kwargs["supplier"])
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+class ArticleTypeSupplierView(mixins.ListModelMixin,
+                              mixins.CreateModelMixin,
+                              generics.GenericAPIView):
+    serializer_class = ArticleTypeSupplierSerializer
+
+    def get_queryset(self):
+        return ArticleTypeSupplier.objects.filter(article_type=self.kwargs["article_type"])
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
