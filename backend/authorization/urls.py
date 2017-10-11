770ac94d46f2f1a005b57f660ac41c23b65664aa
rhbvkleef <rhbvkleef@gmail.com>
2017-10-11 22:48:24 +0200

    Fixed api endpoint and added A GODDAMN DEFAULT GRAVATAR

--- backend/authorization/urls.py
+++ backend/authorization/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 
 from authorization import views
 
@@ -6,4 +7,5 @@ from authorization import views
 urlpatterns = (
     url(r'login/', views.Login.as_view()),
     url(r'logout/', views.Logout.as_view()),
+    url(r'profile/', views.UserProfile.as_view()),
 )
--- backend/authorization/views.py
+++ backend/authorization/views.py
@@ -8,7 +8,6 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
-
 User = settings.AUTH_USER_MODEL
 
 
@@ -40,6 +39,31 @@ class Login(ObtainAuthToken):
             'user': {
                 'username': user.username,
                 'permissions': user.get_all_permissions(),
-                'gravatarUrl': '//failurl',
+                'gravatarUrl': 'https://www.gravatar.com/avatar/00000000000000000000000000000000',
+            },
+        })
+
+
+class UserProfile(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserProfile, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request):
+        tok = Token.objects.filter(key=request.POST['token']).all()
+        if len(tok) <= 0:
+            return JSONResponse({
+                'error': 'No user found',
+                'token': request.POST['token'],
+            })
+
+        user = tok.get().user
+        return JSONResponse({
+            'user': {
+                'username': user.username,
+                'firstName': user.first_name,
+                'lastName': user.last_name,
+                'email': user.email,
+                'gravatarUrl': 'https://www.gravatar.com/avatar/00000000000000000000000000000000',
             },
         })
