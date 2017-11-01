ad6a8c6f1a2c5a83e71f152844f548d0cda26756
rhbvkleef <rhbvkleef@gmail.com>
2017-11-02 00:18:45 +0100

    Altered interface of permission mixin and decorator

--- backend/authorization/decorators.py
+++ backend/authorization/decorators.py
@@ -7,17 +7,18 @@ from rest_framework.authtoken.models import Token
 from swipe import settings
 
 
-def authenticate(mode, perm=None):
+def authenticate(username=False, token=False, perm=None):
     def _auth_wrapper_func(view):
         def _wrapper_func(request, *args, **kwargs):
-            if mode == 'username,token' or mode == 'token,username':
-                tokens = Token.objects.filter(key=request.POST['token']).filter(user__username=request.POST['username'])
-            elif mode == 'username':
-                tokens = Token.objects.filter(user__username=request.POST['username'])
-            elif mode == 'token':
-                tokens = Token.objects.filter(key=request.POST['token'])
-            else:
+            if not token and not username:
                 raise PermissionDenied
+
+            tokens = Token.objects
+            if username:
+                tokens = tokens.filter(user__username=request.POST['username'])
+
+            if token:
+                tokens = tokens.filter(key=request.POST['token'])
 
             if tokens.count() == 1 and tokens.first().created + datetime.timedelta(
                     hours=settings.AUTH_TOKEN_VALID_TIME_HOURS) > timezone.now():
