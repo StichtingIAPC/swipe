03ca4462dd130818b40471d357ab5a406a5f975b
MaukWM <maukmul@gmail.com>
2017-11-01 22:45:38 +0100

    refactored code for post requests

--- backend/logistics/views.py
+++ backend/logistics/views.py
@@ -14,6 +14,7 @@ from logistics.serializers import SupplierOrderSerializer, SupplierOrderLineSeri
     StockWishSerializer
 from money.models import Cost
 from supplier.models import Supplier
+from tools.util import raiseif
 
 
 class SupplierOrderListView(mixins.ListModelMixin,
@@ -24,29 +25,53 @@ class SupplierOrderListView(mixins.ListModelMixin,
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
-    # move this post method to SupplierOrderView?
     def post(self, request, *args, **kwargs):
-        self.user = User.objects.get(id=request.data.get("user_modified"))
-        self.articles_ordered = request.data.get("articles_ordered")
-        resultset = []
-        for article_id, number, cost in self.articles_ordered:
-            entry = []
-            article = ArticleType.objects.get(id=article_id)
-            cost = Cost(amount=Decimal(cost.get("amount")), currency=cost.get("currency"), use_system_currency=cost.get("use_system_currency"))
-            entry.append(article)
-            entry.append(number)
-            entry.append(cost)
-            resultset.append(entry)
-        self.allow_different_currency = request.data.get("allow_different_currency")
-        self.supplier = Supplier.objects.get(id=request.data.get("supplier"))
-        supplierorder = SupplierOrder.create_supplier_order(user_modified=self.user,
-                                                            supplier=self.supplier,
-                                                            articles_ordered=resultset,
-                                                            allow_different_currency=self.allow_different_currency)
-        # was not able to directly serialize... maybe try to find a better way to do this?
-        supplierorder_json = json.dumps({"user_created": supplierorder.user_created.id,
-                                        "supplier": supplierorder.supplier.id})
-        return HttpResponse(content=supplierorder_json, content_type="application/json")
+        supplierorder_request = SupplierOrderRequest(user_modified=request.data.get("user_modified"),
+                                                     articles_ordered_list=request.data.get("articles_ordered"),
+                                                     allow_different_system_currency=request.data.get("allow_different_currency"),
+                                                     supplier=request.data.get("supplier"))
+        supplierorder_representation = SupplierOrderSerializer(instance=supplierorder_request.create_supplierorder_request()).data
+        return HttpResponse(content=json.dumps(supplierorder_representation), content_type="application/json")
+
+class SupplierOrderRequest:
+
+    def __init__(self, user_modified: int, articles_ordered_list: list, supplier: int, allow_different_system_currency: bool):
+        self.user_modified = user_modified
+        raiseif(not isinstance(user_modified, int), "User is not of type int")
+        self.supplier = supplier
+        raiseif(not isinstance(supplier, int), "Supplier is not of type int")
+        self.allow_different_system_currency = allow_different_system_currency
+        raiseif(not isinstance(allow_different_system_currency, bool), "allow_different_system_currency is not of type bool")
+        self.articles_ordered = []
+        for article_id, amount, cost in articles_ordered_list:
+            self.articles_ordered.append(SupplierOrderRequest.OrderedArticle(article_id=article_id,
+                                                                             amount=amount,
+                                                                             cost=cost))
+
+    def create_supplierorder_request(self):
+        user = User.objects.get(id=self.user_modified)
+        supplier = Supplier.objects.get(id=self.supplier)
+        arts_ordered = []
+        for article_ordered in self.articles_ordered:
+            arts_ordered.append(article_ordered.to_model_data())
+        return SupplierOrder.create_supplier_order(user_modified=user, supplier=supplier, articles_ordered=arts_ordered,
+                                                   allow_different_currency=self.allow_different_system_currency)
+
+
+    class OrderedArticle:
+
+        def __init__(self, article_id: int, amount: int, cost):
+            self.article_id = article_id
+            raiseif(not isinstance(article_id, int), "article_id is not of type int")
+            self.amount = amount
+            raiseif(not isinstance(amount, int), "amount is not of type int")
+            self.cost = cost
+
+        def to_model_data(self):
+            article = ArticleType.objects.get(pk=self.article_id)
+            cost = Cost(amount=Decimal(self.cost.get("amount")), currency=self.cost.get("currency"),
+                        use_system_currency=self.cost.get("use_system_currency"))
+            return [article, self.amount, cost]
 
 
 class SupplierOrderView(mixins.RetrieveModelMixin,
@@ -127,18 +152,41 @@ class StockWishView(mixins.CreateModelMixin,
     serializer_class = StockWish
 
     def post(self, request, *args, **kwargs):
-        self.user = User.objects.get(id=request.data.get("user_modified"))
-        self.articles_ordered = request.data.get("articles_ordered")
-        resultset = []
-        for article_id, number in self.articles_ordered:
-            entry = []
-            article = ArticleType.objects.get(id=article_id)
-            entry.append(article)
-            entry.append(number)
-            resultset.append(entry)
-        stockwish = StockWishSerializer(StockWish.create_stock_wish(user_modified=self.user, articles_ordered=resultset)).data
-        return HttpResponse(content=json.dumps(stockwish), content_type="application/json")
+        stockwish_request = StockWishRequest(user=request.data.get("user_modified"),
+                                             articles_ordered_list=request.data.get("articles_ordered"))
+        stockwish_representation = StockWishSerializer(instance=stockwish_request.create_stockwish_request()).data
+        return HttpResponse(content=json.dumps(stockwish_representation), content_type="application/json")
 
+
+class StockWishRequest:
+
+    def __init__(self, user: int, articles_ordered_list: list,):
+        raiseif(not isinstance(user, int), "User is not of type int")
+        self.user = user
+        self.articles_ordered = []
+        for article_id, amount_ordered in articles_ordered_list:
+            self.articles_ordered.append(StockWishRequest.ArticlesOrderedInformation(
+                article_id=article_id, amount_ordered=amount_ordered
+            ))
+
+    def create_stockwish_request(self):
+        user = User.objects.get(id=self.user)
+        arts_ordered = []
+        for article_ordered in self.articles_ordered:
+            arts_ordered.append(article_ordered.to_model_data())
+        return StockWish.create_stock_wish(user_modified=user, articles_ordered=arts_ordered)
+
+    class ArticlesOrderedInformation:
+
+        def __init__(self, article_id: int, amount_ordered: int):
+            self.article_id = article_id
+            raiseif(not isinstance(article_id, int), "article_id is not of type int")
+            self.amount_ordered = amount_ordered
+            raiseif(not isinstance(amount_ordered, int), "amount_ordered is not of type int")
+
+        def to_model_data(self):
+            article = ArticleType.objects.get(pk=self.article_id)
+            return (article, self.amount_ordered)
 
 
 class StockWishTableLogListView(mixins.ListModelMixin,
