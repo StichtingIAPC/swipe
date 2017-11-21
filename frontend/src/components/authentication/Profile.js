a4959996743b58cc9d467dfdf65cefd6023c41d7
Christiaan Biesterbosch <cbiesterbosch@iapc.utwente.nl>
2017-11-21 22:02:37 +0100

    Fix formatting errors in Profile.js

--- frontend/src/components/authentication/Profile.js
+++ frontend/src/components/authentication/Profile.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { push } from 'react-router-redux';
 
 class Profile extends React.Component {
-
 	render() {
 		return (
-		    <div className="row">
-                <div className="col-sm-1">
-                    <img className="user-image" title="Gravatar" src={this.props.user.gravatarUrl} />
-                </div>
-                <div className="col-sm-8">
-                    <span>Username: </span>{this.props.user.username}
-                </div>
-                <div className="col-sm-8">
-                    <span>Email address: </span>{this.props.user.email}
-                </div>
-                <div className="col-sm-8">
-                    <span>Name: {this.props.user.firstName} {this.props.user.lastName} </span>
-                </div>
-            </div>
-        );
+			<div className="row">
+				<div className="col-sm-1">
+					<img className="user-image" title="Gravatar" src={this.props.user.gravatarUrl} />
+				</div>
+				<div className="col-sm-8">
+					<span>Username: </span>{this.props.user.username}
+				</div>
+				<div className="col-sm-8">
+					<span>Email address: </span>{this.props.user.email}
+				</div>
+				<div className="col-sm-8">
+					<span>Name: {this.props.user.firstName} {this.props.user.lastName} </span>
+				</div>
+			</div>
+		);
 	}
 }
 
 export default connect(
 	state => ({
-        user: state.auth.currentUser,
-        isAuthenticated: state.auth.currentUser !== null,
-    }),
-    null
+		user: state.auth.currentUser,
+		isAuthenticated: state.auth.currentUser !== null,
+	}),
+	null
 )(Profile);
