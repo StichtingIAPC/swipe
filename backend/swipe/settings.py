e90c2ee8d0ab164e2ecd7df0983f1a1737a49ce9
Léon van der Kaap <leonkaap@gmail.com>
2017-12-12 22:19:50 +0100

    Start on postgres support, does not work yet as some migration files fail.

--- backend/swipe/settings.py
+++ backend/swipe/settings.py
@@ -108,8 +108,12 @@ WSGI_APPLICATION = 'swipe.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'swipe',
+        'USER': 'postgres',
+        'POSTGRES': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
