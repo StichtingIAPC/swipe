953d5c3ed6565ac6b650f1057eae996c59f57960
nander <nanderv@gmail.com>
2018-01-18 22:57:59 +0100

    Removed useless lines.

--- frontend/src/components/sales/receipt/Totals.js
+++ frontend/src/components/sales/receipt/Totals.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { connectMixin, fetchStateRequirementsFor } from '../../../core/stateRequirements';
-import { registers } from '../../../state/register/registers/actions.js';
 
-import { currencies } from '../../../state/money/currencies/actions.js';
-import { paymentTypes } from '../../../state/register/payment-types/actions.js';
-import { articles } from '../../../state/assortment/articles/actions';
-import { addToSalesListAction } from '../../../state/sales/sales/actions';
-import { getArticleNameById, getCount, getSalesTotal } from "../../../state/assortment/articles/selectors";
-import { stock } from "../../../state/stock/actions";
-import MoneyAmount from "../../money/MoneyAmount";
+import { getSalesTotal } from '../../../state/assortment/articles/selectors';
+import { stock } from '../../../state/stock/actions';
+import MoneyAmount from '../../money/MoneyAmount';
 
 class SalesList extends React.Component {
 	componentWillMount() {
@@ -17,17 +12,16 @@ class SalesList extends React.Component {
 	}
 
 	render() {
-		const { state } = this.props;
-		const total = getSalesTotal(state);
-		total.currency = total.currency || "EUR";
-		total.amount = total.amount || 0;
+		const { total } = this.props;
+		if (!total)
+			return null;
 		return (
 
 			<tr key = "TOTAL">
 				<td>TOTAL</td>
 				<td></td>
 				<td></td>
-				<td><MoneyAmount money={total}/></td>
+				<td><MoneyAmount money={total} /></td>
 			</tr>
 		);
 	}
@@ -36,23 +30,11 @@ class SalesList extends React.Component {
 export default connect(
 	state => ({
 		...connectMixin({
-			 register: {
-				 registers,
-				 paymentTypes,
-			 },
-			 money: {
-				 currencies,
-			 },
-			 article: {
-				 articles,
-			 },
 			 sales: {
 				 stock,
 			 },
 		 }, state
 		),
-		sales: state.sales.sales.sales,
-		state: state,
+		total: getSalesTotal(state),
 	})
-	, { addArticle:  (evt, count) => addToSalesListAction(evt, count), dispatch: args => args }
 )(SalesList);
