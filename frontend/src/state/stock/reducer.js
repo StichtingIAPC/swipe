52cdec451eb41ddbb350ad0ad2d164a7724b4d54
nander <nanderv@gmail.com>
2018-01-18 23:45:50 +0100

    rewrote to use combineReducers.

--- frontend/src/state/stock/reducer.js
+++ frontend/src/state/stock/reducer.js
@@ -1,34 +1,16 @@
 import { STOCK_FETCH_DONE, STOCK_FETCH_FAILED, STOCK_FETCH_START } from './actions';
-const initialState = {
-	stock: [],
-	fetching: false,
-	fetchError: null,
-	inputError: null,
-};
+import {
+	booleanControlReducer, collectReducers, resetFieldReducer,
+	setFieldReducer
+} from '../../tools/reducerComponents';
+import { combineReducers } from 'redux';
 
-export default function stockReducer(state = initialState, action) {
-	if (action.type === STOCK_FETCH_START) {
-		return {
-			...state,
-			fetching: true,
-			inputError: null,
-		};
-	}
-	if (action.type === STOCK_FETCH_DONE) {
-		return {
-			...state,
-			fetching: false,
-			stock: action.stock,
-			fetchError: null,
-		};
-	}
 
-	if (action.type === STOCK_FETCH_FAILED) {
-		return {
-			...state,
-			fetchError: action.error,
-			fetching: false,
-		};
-	}
-	return state;
-}
+export default combineReducers({
+	stock: setFieldReducer([ STOCK_FETCH_DONE ], [], 'stock'),
+	fetching: booleanControlReducer({
+		[STOCK_FETCH_START]: true,
+		[STOCK_FETCH_DONE]: false,
+		[STOCK_FETCH_FAILED]: false }, false),
+	fetchError: collectReducers(resetFieldReducer([ STOCK_FETCH_DONE ], null), setFieldReducer([ STOCK_FETCH_FAILED ], null, 'error')),
+});
