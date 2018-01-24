144e14dab8a8f28a49ce60256bae5c728c2c99ba
noverkamp <noverkamp@iapc.utwente.nl>
2018-01-24 01:02:56 +0100

    This commit fixes the bugs mentioned in #183. It removes the previous input method and replaces it with a more intuitive approach where when you type 1 or 1.0 1.000000 it all gives the same result (namely 1) It also supports .1 and will display the thousand separator.
    It also makes a small change in CurrencyEdit.js such that it expects a
    value in onChange instead of an event.
    
    It however introduces a few bugs of its own
    -It sends the thousand separator to the backend, which can not handle
     that.

--- frontend/src/components/forms/MoneyField.js
+++ frontend/src/components/forms/MoneyField.js
@@ -1,41 +1,61 @@
 import React from 'react';
+import numeral from 'numeral'
 
 /**
  * Created by Matthias on 30/11/2016.
  */
 
 export default class MoneyField extends React.Component {
-	render() {
-		const { currency, value, onChange, children, ...restProps } = this.props;
 
-		return (
-			<div className="input-group">
-				<span className="input-group-addon">{currency.symbol}</span>
-				<input
-					type="text"
-					className="form-control"
-					value={MoneyField.valueToString(value, currency)}
-					placeholder = {MoneyField.getPlaceholder(currency)}
-					onChange={onChange}
-					{...restProps} />
-				{children}
-			</div>
-		);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            format: getFormat(props.currency)
+        };
+        if (props.value !== "") {
+            this.state.stringValue = numeral(props.value).format(this.state.format);
+        } else {
+            this.state.stringValue = "";
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-	static valueToString(value, currency) {
-		if (isNaN(Number(value.replace('.', '')))) {
-			return value;
-		}
 
-		let _value = value.replace('.', '');
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.currency !== this.props.currency) {
+            this.setState({format: getFormat(nextProps.currency)})
+        }
+    }
 
-		_value = Number(_value) / Math.pow(10, currency.digits);
-		_value = _value === 0 ? '' : _value.toFixed(currency.digits);
-		return _value;
-	}
 
-	static getPlaceholder(currency) {
-		return `0.${'0'.repeat(currency.digits)}`;
-	}
+    handleChange(event) {
+        const newValue = event.target.value;
+        this.setState({stringValue: newValue}, () =>
+            this.props.onChange(numeral(newValue).format(this.state.format)))
+    }
+
+    render() {
+        const { currency, children, value, onChange, ...restProps } = this.props;
+        return (
+            <div className="input-group">
+                <span className="input-group-addon">{currency.symbol}</span>
+                <input
+                    type="text"
+                    className="form-control"
+                    value={this.state.stringValue}
+                    onChange={this.handleChange}
+                    {...restProps}
+                />
+                {children}
+            </div>
+        );
+    }
+}
+
+function getFormat(currency) {
+    let format = "0,0";
+    if (currency.digits > 0) {
+        format = format + "." + "0".repeat(currency.digits)
+    }
+    return format
 }
