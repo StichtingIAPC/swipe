e5c467a6036763d5bf50d99c1137ff5cab86c291
Simon Struck <simonstruck70@gmail.com>
2018-01-25 23:25:57 +0100

    Add status indicator to sales link that displays if a register is open
    Fix django missing module 'compressor'

--- frontend/src/state/register/reducer.js
+++ frontend/src/state/register/reducer.js
@@ -2,8 +2,12 @@ import { combineReducers } from 'redux';
 
 import paymentTypes from './payment-types/reducer.js';
 import registers from './registers/reducer.js';
+import { setFieldReducer } from '../../tools/reducerComponents';
 
 export default combineReducers({
 	paymentTypes,
 	registers,
+	open: setFieldReducer([
+		'REGISTER_OPEN_FETCH_SUCCESS',
+	], false, 'isOpen'),
 });
