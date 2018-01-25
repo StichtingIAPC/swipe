212bf3afbd626aa402dc37d7b209607ed5c1985f
noverkamp <noverkamp@iapc.utwente.nl>
2018-01-25 12:15:44 +0100

    Fixes Linting problems
    In MoneyField.js

--- frontend/src/components/forms/MoneyField.js
+++ frontend/src/components/forms/MoneyField.js
@@ -1,73 +1,75 @@
 import React from 'react';
-import numeral from 'numeral'                                                                                           // Numeral is a library for parsing text representations of
-                                                                                                                        // numbers and formatting numbers as text
+import numeral from 'numeral';
+// Numeral is a library for parsing text representations of
+// numbers and formatting numbers as text
 
 /**
  * Created by Matthias on 30/11/2016.
  */
 
-export default class MoneyField extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            displayFormat: getFormat(props.currency, true),                                                             // Format used for displaying the value to the user (1,000.0)
-            format: getFormat(props.currency, false),                                                                   // Format used to store the value. (1000.0)
-
-        };
-        if (props.value === "") {
-            this.state.displayString = "";                                                                              // String storing the string currently displaying
-        } else {
-            this.state.displayString = numeral(props.value).format(this.state.displayFormat);                           // Converts the value passed on to a number, and formats it for displaying
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleBlur = this.handleBlur.bind(this);
-    }
-
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.currency !== this.props.currency) {
-            this.setState({displayFormat: getFormat(nextProps.currency, true)})
-        }
-    }
-
-
-    handleChange(event) {
-        const newValue = event.target.value;
-        this.setState({displayString: newValue}, () =>
-            this.props.onChange(numeral(newValue).format(this.state.format)))                                           // Reformats the value for storing and passes on
-    }
-
-    handleBlur(event) {
-        this.setState({displayString: numeral(this.state.displayString).format(this.state.displayFormat)})              // On lost focus reformat the displayString for displaying
-    }
-
-    render() {
-        const { currency, children, value, onChange, ...restProps } = this.props;
-        return (
-            <div className="input-group">
-                <span className="input-group-addon">{currency.symbol}</span>
-                <input
-                    type="text"
-                    className="form-control"
-                    value={this.state.displayString}
-                    onChange={this.handleChange}
-                    onBlur={this.handleBlur}
-                    {...restProps}
-                />
-                {children}
-            </div>
-        );
-    }
-}
 
 function getFormat(currency, hasThousandsSeparator) {
-    let format = "0";
-    if (hasThousandsSeparator) {
-        format = format + ",0";
-    }
-    if (currency.digits > 0) {
-        format = format + "." + "0".repeat(currency.digits);
-    }
-    return format
+	let format = "0";
+	if (hasThousandsSeparator) {
+		format = format + ",0";
+	}
+	if (currency.digits > 0) {
+		format = format + "." + "0".repeat(currency.digits);
+	}
+	return format;
+}
+
+export default class MoneyField extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			displayFormat: getFormat(props.currency, true),																// Format used for displaying the value to the user (1,000.0)
+			format: getFormat(props.currency, false),																	// Format used to store the value. (1000.0)
+
+		};
+		if (props.value === "") {
+			this.state.displayString = "";																				// String storing the string currently displaying
+		} else {
+			this.state.displayString = numeral(props.value).format(this.state.displayFormat);							// Converts the value passed on to a number, and formats it for displaying
+		}
+		this.handleChange = this.handleChange.bind(this);
+		this.handleBlur = this.handleBlur.bind(this);
+	}
+
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.currency !== this.props.currency) {
+			this.setState({displayFormat: getFormat(nextProps.currency, true)});
+		}
+	}
+
+
+	handleChange(event) {
+		const newValue = event.target.value;
+		this.setState({displayString: newValue}, () =>
+			this.props.onChange(numeral(newValue).format(this.state.format)));											// Reformats the value for storing and passes on
+	}
+
+	handleBlur() {
+		this.setState({displayString: numeral(this.state.displayString).format(this.state.displayFormat)});				// On lost focus reformat the displayString for displaying
+	}
+
+	render() {
+		const {currency, children, value, onChange, ...restProps} = this.props;
+		return (
+			<div className="input-group">
+				<span className="input-group-addon">{currency.symbol}</span>
+				<input
+					type="text"
+					className="form-control"
+					value={this.state.displayString}
+					onChange={this.handleChange}
+					onBlur={this.handleBlur}
+					{...restProps}/>
+				{children}
+			</div>
+		);
+	}
 }
