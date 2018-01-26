6c8596ac77bb108949ef5f02ecfe616a7193bdc2
Rolf H. B. van Kleef <rkleef@iapc.utwente.nl>
2018-01-26 14:37:07 +0100

    Added budduns to externalise!

--- frontend/src/components/logistics/Externalise.js
+++ frontend/src/components/logistics/Externalise.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import FontAwesome from '../tools/icons/FontAwesome';
 
 import { fetchAll as fetchAllExternalisations } from '../../state/logistics/externalise/actions';
 
@@ -33,14 +35,48 @@ class Externalise extends Component {
 	}
 
 	render() {
-		return <BootstrapTable
-			keyField="id"
-			columns={this.cols}
-			data={this.props.externalises} />;
+		return (
+			<div className="row">
+				<div className="col-md-1">
+					<div className="box">
+						<div className="box-header">
+							<div className="box-title">
+								Externalisations
+							</div>
+							<div className="box-tools">
+								<div className="btn-group">
+									<a
+										className={`btn btn-sm btn-default ${this.props.loading ? 'disabled' : ''}`}
+										title="Refresh"
+										onClick={this.props.fetchExternalisations}>
+										<FontAwesome icon={`refresh ${this.props.loading ? 'fa-spin' : ''}`} />
+									</a>
+									<Link
+										className="btn btn-default btn-sm"
+										to="/logistics/externalise/create/"
+										title="Create new register">
+										<FontAwesome icon="plus" />
+									</Link>
+								</div>
+							</div>
+						</div>
+						<div className="box-body">
+							<BootstrapTable
+								keyField="id"
+								columns={this.cols}
+								data={this.props.externalises} />
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
 
 export default connect(
-	state => ({ externalises: state.logistics.externalise.externalisations }),
+	state => ({
+		externalises: state.logistics.externalise.externalisations,
+		loading: state.logistics.externalise.loading,
+	}),
 	{ fetchExternalisations: fetchAllExternalisations }
 )(Externalise);
