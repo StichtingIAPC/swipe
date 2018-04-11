fc7a39ebc6563945ddff599e27246ca8bb10dfab
Nander Voortman <nvoortman@iapc.utwente.nl>
2018-04-11 20:55:08 +0200

    Fix eslint.

--- frontend/src/state/logistics/externalise/sagas.js
+++ frontend/src/state/logistics/externalise/sagas.js
@@ -5,37 +5,36 @@ import store from '../../store';
 import * as api from './api';
 import * as actions from './actions';
 import { cleanErrorMessage } from '../../../tools/sagaHelpers';
-import { getExternalisationActiveObject, getExternalisationLoading, getExternalisationPopulated } from './selectors';
+import { getExternalisationActiveObject, getExternalisationLoading } from './selectors';
 import { isMoney, validate, validator } from '../../../tools/validations/validators';
-import { setLoadingAction } from "./actions";
 
-export function* fetchAll () {
-	if (yield select (getExternalisationLoading))
+export function* fetchAll() {
+	if (yield select(getExternalisationLoading)) {
 		return;
-	yield put (setLoadingAction ());
+	}
+	yield put(actions.setLoadingAction());
 	try {
+		const externalizations = yield (yield call(api.getAll)).json();
 
-		const externalizations = yield (yield call (api.getAll)).json ();
-
-		const exts = [].concat.apply ([], externalizations.map (e => e.externaliseline_set.map (en => ({
+		const exts = [].concat.apply([], externalizations.map(e => e.externaliseline_set.map(en => ({
 			memo: e.memo,
 			count: en.count,
 			amount: en.cost,
 			article: en.article_type,
 		}))));
 
-		yield put (actions.fetchAllSuccess (exts));
+		yield put(actions.fetchAllSuccess(exts));
 	} catch (e) {
-		console.error (e);
-		yield put (actions.fetchAllError (e));
+		console.error(e);
+		yield put(actions.fetchAllError(e));
 	} finally {
-		yield put (actions.fetchAllFinally ());
+		yield put(actions.fetchAllFinally());
 	}
 }
 
-export function* create ( { externalise } ) {
+export function* create({ externalise }) {
 	const document = {
-		externaliseline_set: externalise.externaliseline_set.map (e => ({
+		externaliseline_set: externalise.externaliseline_set.map(e => ({
 			...e,
 			// eslint-disable-next-line
 			amount: undefined,
@@ -43,59 +42,60 @@ export function* create ( { externalise } ) {
 			article: e.article.id,
 		})),
 		memo: externalise.memo,
-		user: store.getState ().auth.currentUser.id,
+		user: store.getState().auth.currentUser.id,
 	};
 
 	try {
-		const newExternalise = yield (yield call (api.post, document)).json ();
+		const newExternalise = yield (yield call(api.post, document)).json();
 
-		yield put (actions.createSuccess (newExternalise));
+		yield put(actions.createSuccess(newExternalise));
 	} catch (e) {
-		yield put (actions.createError (cleanErrorMessage (e)));
+		yield put(actions.createError(cleanErrorMessage(e)));
 	} finally {
-		yield put (actions.createFinally ());
+		yield put(actions.createFinally());
 	}
 }
 
-export function* createSuccess () {
-	yield put (actions.fetchAllAction ());
-	yield put (push ('/logistics/externalise'));
+export function* createSuccess() {
+	yield put(actions.fetchAllAction());
+	yield put(push('/logistics/externalise'));
 }
 
 const validations = [
-	validator ('memo', 'Memo', memo => memo.length > 3 ? null : () => ({
+	validator('memo', 'Memo', memo => memo.length > 3 ? null : () => ({
 		type: 'error',
 		text: 'Memo field not long enough',
 	})),
-	validator ('memo', 'Memo', memo => memo.length < 24 ? null : () => ({
+	validator('memo', 'Memo', memo => memo.length < 24 ? null : () => ({
 		type: 'warning',
-		text: 'Memo field too long'
+		text: 'Memo field too long',
 	})),
-	validator ('externaliseline_set', 'Externalize set', set => set.reduce (( accumulator, current ) => accumulator && isMoney (current.amount.amount), true) ? null : () => ({
+	validator('externaliseline_set', 'Externalize set', set => set.reduce((accumulator, current) => accumulator && isMoney(current.amount.amount), true) ? null : () => ({
 		type: 'error',
 		text: 'Some money input is invalid.',
 	})),
-	validator ('externaliseline_set', 'Externalize set', set => set.reduce (( accumulator, current ) => accumulator && current.count > 0, true) ? null : () => ({
+	validator('externaliseline_set', 'Externalize set', set => set.reduce((accumulator, current) => accumulator && current.count > 0, true) ? null : () => ({
 		type: 'error',
 		text: 'Some product count is invalid.',
 	})),
-	validator ('externaliseline_set', 'Externalize set', set => set.reduce (( accumulator, current ) => accumulator && current.article, true) ? null : () => ({
+	validator('externaliseline_set', 'Externalize set', set => set.reduce((accumulator, current) => accumulator && current.article, true) ? null : () => ({
 		type: 'error',
 		text: 'Some product is not set.',
 	})),
 ];
 
-export function* externalizeValidator () {
-	const current = yield select (getExternalisationActiveObject);
-	const res = validate (current, validations);
-	yield put (actions.setValidations (res));
+export function* externalizeValidator() {
+	const current = yield select(getExternalisationActiveObject);
+	const res = validate(current, validations);
+
+	yield put(actions.setValidations(res));
 }
 
-export default function* saga () {
-	yield takeEvery (actions.SET_FIELD_ACTION, externalizeValidator);
-	yield takeEvery (actions.CREATE_ACTION, externalizeValidator);
+export default function* saga() {
+	yield takeEvery(actions.SET_FIELD_ACTION, externalizeValidator);
+	yield takeEvery(actions.CREATE_ACTION, externalizeValidator);
 
-	yield takeLatest (actions.FETCH_ALL_ACTION, fetchAll);
-	yield takeEvery (actions.CREATE_ACTION, create);
-	yield takeLatest (actions.CREATE_SUCCESS, createSuccess);
+	yield takeLatest(actions.FETCH_ALL_ACTION, fetchAll);
+	yield takeEvery(actions.CREATE_ACTION, create);
+	yield takeLatest(actions.CREATE_SUCCESS, createSuccess);
 }
