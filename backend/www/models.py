fe00f1b811b8ab7dd4ee523e093f3f616ec4ee8c
Léon van der Kaap <leonkaap@gmail.com>
2018-05-06 23:06:50 +0200

    Implemented the Swipe version of login requirement.

--- backend/www/models.py
+++ backend/www/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
 
-# Create your models here.
+
+class SwipeLoginRequired(LoginRequiredMixin):
+    """
+    The Swipe implementation of checking if the user who tried to access an endpoint was logged in. Any view that
+    should be protected by user authentication should inherit from this class FIRSTLY. If done later in the inheritance
+    chain it might be skipped.
+    """
+
+    def __init__(self):
+        super(SwipeLoginRequired, self).__init__()
+        self.raise_exception = True
+
+    def handle_no_permission(self):
+        if self.raise_exception:
+            return HttpResponse('401 Unauthorized', status=401)
