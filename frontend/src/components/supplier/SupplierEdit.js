5cb97702692cd83b88bde7563b70445d60810124
Nander Voortman <nvoortman@iapc.utwente.nl>
2018-05-15 21:51:13 +0200

    Revert needless fixes.

--- frontend/src/components/supplier/SupplierEdit.js
+++ frontend/src/components/supplier/SupplierEdit.js
@@ -45,7 +45,7 @@ class SupplierEdit extends React.Component {
 	setNotes = ({ target: { value }}) => this.props.setSupplierField('notes', value);
 	setSearch_url = ({ target: { value }}) => this.props.setSupplierField('search_url', value);
 
-	// eslint-disable-next-line complexity
+
 	render() {
 		const { supplier } = this.props;
 		const NEW = supplier.id === null;
