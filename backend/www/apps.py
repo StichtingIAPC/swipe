2bf9e152ad21a30d887f55d60e8ba6b5bd0594fa
Léon van der Kaap <leonkaap@gmail.com>
2018-05-17 21:32:36 +0200

    Escape when something goes wrong(like a bootstrapping problem).

--- backend/www/apps.py
+++ backend/www/apps.py
@@ -17,16 +17,19 @@ class PermissionApplicationConfig(AppConfig):
     def ready(self):
         from crm.models import SwipePermission
         from www.permissions import PERMISSION_LIST
-        all_permissions = SwipePermission.objects.all()  # type: list[SwipePermission]
-        if all_permissions.count() != len(PERMISSION_LIST):
-            permission_map = {}
-            string_set = set(PERMISSION_LIST)
-            for perm in all_permissions:
-                if perm.name not in string_set:
-                    perm.delete()
-                else:
-                    permission_map[perm.name] = perm
-            for perm_string in PERMISSION_LIST:
-                if not permission_map.get(perm_string):
-                    new_permission = SwipePermission(name=perm_string)
-                    new_permission.save()
+        try:
+            all_permissions = SwipePermission.objects.all()  # type: list[SwipePermission]
+            if all_permissions.count() != len(PERMISSION_LIST):
+                permission_map = {}
+                string_set = set(PERMISSION_LIST)
+                for perm in all_permissions:
+                    if perm.name not in string_set:
+                        perm.delete()
+                    else:
+                        permission_map[perm.name] = perm
+                for perm_string in PERMISSION_LIST:
+                    if not permission_map.get(perm_string):
+                        new_permission = SwipePermission(name=perm_string)
+                        new_permission.save()
+        except Exception as e:
+            pass
