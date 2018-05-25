7cfbf2292cb724a41fdc63e26ffb31b404c49c2b
Simon Struck <simonstruck70@gmail.com>
2018-05-25 23:42:24 +0200

    Add custom reducers to crudReducers

--- frontend/src/tools/reducerComponents.js
+++ frontend/src/tools/reducerComponents.js
@@ -74,3 +74,50 @@ export const collectReducers = (...reducers) =>
 
 export const resetFieldReducer = (types, defaultValue) =>
 	(state = defaultValue, action) => types.includes(action.type) ? defaultValue : state;
+
+const subMerge = (resultObject, objectA, objectB, mergeFunction) => {
+	const keys = {};
+
+	for (const key in objectA) {
+		if (objectA.hasOwnProperty(key)) {
+			keys[key] = true;
+		}
+	}
+	for (const key in objectB) {
+		if (objectB.hasOwnProperty(key)) {
+			keys[key] = true;
+		}
+	}
+	for (const key in keys) {
+		if (!objectA.hasOwnProperty(key)) {
+			resultObject[key] = objectB[key];
+		} else if (!objectB.hasOwnProperty(key) || objectA[key] == objectB[key]) {
+			resultObject[key] = objectA[key];
+		} else if (typeof objectA[key] === 'object' && typeof objectB[key] === 'object' && !Array.isArray(objectA[key]) && !Array.isArray(objectB[key])) {
+			resultObject[key] = {};
+			subMerge(resultObject[key], objectA[key], objectB[key], mergeFunction);
+		} else {
+			resultObject[key] = mergeFunction(objectA[key], objectB[key]);
+		}
+	}
+};
+/**
+ * Recursively merges two objects and executes given function if it can't merge intuitively.
+ *
+ * Examples:
+ * Invoking this function with `{a: 1}` and `{b: 2}` would return `{a: 1, b: 2}`.
+ * Invoking this function with `{a: [1]}` and `{a: [2], b: 2}` would call the merge function on `a`.
+ * By default, it would concatinate the objects and return `{a: '12', b: 2}` as the merged object.
+ *
+ * @param objectA Object you want to merge with objectB.
+ * @param objectB Object you want to merge with objectA.
+ * @param mergeFunction Function that will get executed, in case function encounters non-intuitively mergable objects [arrays, functions, other incompatible types] function
+ * will receive two parameters and its return value will be placed where the two parameters would have been inserted.
+ * By default this function is simply `(a, b) => a + b`
+ */
+export const mergeObjects = (objectA, objectB, mergeFunction = (a, b) => a + b) => {
+	const resultObject = {};
+
+	subMerge(resultObject, objectA, objectB, mergeFunction);
+	return resultObject;
+};
