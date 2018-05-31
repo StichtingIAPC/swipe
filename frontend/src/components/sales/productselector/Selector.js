54d1cf925093fdf2a26f006344df16cb804f1c51
Rolf H. B. van Kleef <rkleef@iapc.utwente.nl>
2018-05-31 21:49:51 +0200

    Fix whitespace

--- frontend/src/components/sales/productselector/Selector.js
+++ frontend/src/components/sales/productselector/Selector.js
@@ -8,65 +8,65 @@ import { getArticleById } from '../../../state/assortment/articles/selectors';
 import { fetchAllArticles } from '../../../state/assortment/articles/actions';
 
 class Selector extends React.Component {
-    static propTypes = {
-        onArticleRemove: PropTypes.func.isRequired,
-        receipt: PropTypes.arrayOf(PropTypes.shape({
-            article: PropTypes.number.isRequired,
-            price: PropTypes.shape({
-                amount: PropTypes.string.isRequired,
-                currency: PropTypes.string.isRequired,
-            }).isRequired,
-            count: PropTypes.number.isRequired,
-        })).isRequired,
-        stock: PropTypes.arrayOf(PropTypes.shape({
-            article: PropTypes.number.isRequired,
-            price: PropTypes.shape({
-                amount: PropTypes.string.isRequired,
-                currency: PropTypes.string.isRequired,
-            }).isRequired,
-            count: PropTypes.number.isRequired,
-        })).isRequired,
-    };
+	static propTypes = {
+		onArticleRemove: PropTypes.func.isRequired,
+		receipt: PropTypes.arrayOf(PropTypes.shape({
+			article: PropTypes.number.isRequired,
+			price: PropTypes.shape({
+				amount: PropTypes.string.isRequired,
+				currency: PropTypes.string.isRequired,
+			}).isRequired,
+			count: PropTypes.number.isRequired,
+		})).isRequired,
+		stock: PropTypes.arrayOf(PropTypes.shape({
+			article: PropTypes.number.isRequired,
+			price: PropTypes.shape({
+				amount: PropTypes.string.isRequired,
+				currency: PropTypes.string.isRequired,
+			}).isRequired,
+			count: PropTypes.number.isRequired,
+		})).isRequired,
+	};
 
-    componentWillMount() {
-        this.props.fetchAllArticles();
-    }
+	componentWillMount() {
+		this.props.fetchAllArticles();
+	}
 
-    render() {
-        console.log(this.props.stock);
-        return <Box>
-            <Box.Header>
-                Products
-            </Box.Header>
-            <Box.Body>
-                <Table responsive striped hover>
-                    <thead>
-                    <tr>
-                        <th>Article</th>
-                        <th>Price</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        this.props.stock.map(item => (
-                            <tr key={item.article} onClick={() => this.props.onArticleAdd(item.id)}>
-                                <td>{this.props.article(item.article).name}</td>
-                                <td><MoneyAmount money={item.price} /></td>
-                            </tr>
-                        ))
-                    }
-                    </tbody>
-                </Table>
-            </Box.Body>
-        </Box>;
-    }
+	render() {
+		console.log(this.props.stock);
+		return <Box>
+			<Box.Header>
+				Products
+			</Box.Header>
+			<Box.Body>
+				<Table responsive striped hover>
+					<thead>
+						<tr>
+							<th>Article</th>
+							<th>Price</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							this.props.stock.map(item => (
+								<tr key={item.article} onClick={() => this.props.onArticleAdd(item.id)}>
+									<td>{this.props.article(item.article).name}</td>
+									<td><MoneyAmount money={item.price} /></td>
+								</tr>
+							))
+						}
+					</tbody>
+				</Table>
+			</Box.Body>
+		</Box>;
+	}
 }
 
 export default connect(
-    state => ({
-        article: getArticleById.bind(null, state),
-    }),
-    {
-        fetchAllArticles,
-    }
+	state => ({
+		article: getArticleById.bind(null, state),
+	}),
+	{
+		fetchAllArticles,
+	}
 )(Selector);
